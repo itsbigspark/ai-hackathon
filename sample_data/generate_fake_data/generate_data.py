e1522cbc2f423600d6a321473dcc58bb55f23f99
Konrad Zdeb <konrad.zdeb@me.com>
2023-04-16 19:45:45 +0100

    Sample Data
    Sample data generation using faker

--- sample_data/generate_fake_data/generate_data.py
+++ sample_data/generate_fake_data/generate_data.py
@@ -2,9 +2,10 @@
 
 import faker
 from typing import Final
-import random
+import datetime
 import pandas as pd
 from tabulate import tabulate
+import random
 
 # Set faker seed
 FAKER_SEED: Final[int] = 123
@@ -14,13 +15,24 @@ DESIRED_ROWS: Final[int] = 100_000
 fake = faker.Faker()
 fake.seed_instance(FAKER_SEED)
 
-def create_fake_rows(num_rows=100) :
-    res = [{
-        "name": fake.name(),
-        "email": fake.email(),
-        "rand_num": random.randint(0, num_rows)
-    } for x in range(num_rows)]
+
+def create_fake_rows(num_rows=100):
+    res = [
+        {
+            "name": fake.name(),
+            "email": fake.email(),
+            "address": fake.address(),
+            "credit_card": fake.credit_card_number(),
+            "transaction_time": fake.date_time_between(
+                start_date=datetime.datetime(2022, 4, 16, 19, 39),
+                end_date=datetime.datetime(2023, 4, 16, 19, 39),
+            ).isoformat(),
+            "tran_amt": random.randint(100,10000)/100.00
+        }
+        for _ in range(num_rows)
+    ]
     return res
 
+
 df = pd.DataFrame(create_fake_rows(DESIRED_ROWS))
-print(tabulate(df, headers='keys', tablefmt='psql'))
+print(tabulate(df.head(n=20), headers="keys", tablefmt="psql"))
