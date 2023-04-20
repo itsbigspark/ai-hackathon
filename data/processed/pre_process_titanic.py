4cae5b3b5b4469407f9799f6fcb467b869d28ecd
Konrad Zdeb <konrad.zdeb@me.com>
2023-04-20 09:02:53 +0100

    Pre-processing
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

--- data/processed/pre_process_titanic.py
+++ data/processed/pre_process_titanic.py
@@ -6,6 +6,7 @@ import pandas as pd
 import pathlib
 import os
 from tabulate import tabulate
+from typing import Final
 
 
 def pre_process_df(csv_path):
@@ -14,22 +15,34 @@ def pre_process_df(csv_path):
     with open(csv_path) as csv_file:
         df = pd.read_csv(
             filepath_or_buffer=csv_file,
+            skipinitialspace=True,
             header=0,
             converters={"Sex": rec_gender, "Embarked": rec_embarked},
             dtype={"Survived": "bool"},
+            usecols=['Pclass', 'Sex', 'Age', 'Parch', 'Fare', 'Cabin', 'Embarked', 'SibSp']
         )
     return df
 
 
-#
+# Paths for the source files
 SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
 DATA_PATH = os.path.join(SCRIPT_DIR, "..", "raw/")
 
-print("Data path: " + DATA_PATH)
-print("Train data:")
+# Decide whether to preview the data
+SHOW_PREVIEW: Final[bool] = True
+
+# Pre-process test and train data
 df_train = pre_process_df(csv_path=os.path.join(DATA_PATH + "train.csv"))
-print(tabulate(df_train.head(), tablefmt="psql", showindex=True))
-print(df_train.dtypes)
-print("Test data:")
 df_test = pre_process_df(csv_path=os.path.join(DATA_PATH + "test.csv"))
-print(tabulate(df_test.head(), tablefmt="psql", showindex=True))
+
+# Show generated tables
+if SHOW_PREVIEW:
+    print("Data path: " + DATA_PATH)
+    print("Test data:")
+    print(tabulate(df_test.head(), tablefmt="psql", showindex=False, headers=df_test.columns))
+    print('Test data column types:')
+    print(df_test.dtypes)
+    print("Train data:")
+    print(tabulate(df_train.head(), tablefmt="psql", showindex=False, headers=df_train.columns))
+    print('Train data column types:')
+    print(df_train.dtypes)
